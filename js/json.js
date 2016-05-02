b98d5defb49b573e2f677c6168ca27db2c7f8400
mingjixiaohui <923139421@qq.com>
2016-05-02 17:03:50 +0800

    //个人简历

---
+++ js/json.js
@@ -0,0 +1,75 @@
+
+var Json = {
+    project:[
+        {
+            name:'炫酷单页面',  //项目名称
+            date:'2015.10', //项目时间
+            detail:'本页面采用HTML5+css3+FullPage完成，主要是用了css3产生页面的特效。用于个人作品展示', //项目介绍
+            image:['works/5.jpg','works/6.jpg','works/7.jpg','works/8.jpg'], //项目图片地址
+            link:'mingjixiaohui.github.io/dan/ ' //项目链接
+
+        },
+        {
+            name:'158mm互动',  //项目名称
+            date:'2016.3', //项目时间
+            detail:'网站说明：此网站是之前的公司作品，开发此网站，我负责与客户沟通，完成整个网站流程，具体流程：一、了解客户需求（业务提供功能模块）；二、绘制网站原型图；三、与客户沟通确认；四、设计效果图；五、与客户确认效果图并相应修改；六、使用HTML+CSS还效果图、完成前端交互（主要采用原生JavaScript完成）；七、将源码交付给程序；八、协助测试上线；', //项目介绍
+            image:['works/1.jpg','works/2.jpg','works/3.jpg','works/4.jpg'], //项目图片地址
+            link:'mingjixiaohui.github.io/meimei/' //项目链接
+        },
+        {
+            name:'巴黎九大',  //项目名称
+            date:'2015.8', //项目时间
+            detail:'高级奢饰品管理培训,是一个培训展示型的企业网站.网站采用的是HTML+CSS完成，是一个典型的企业网站，用于个人作品展示', //项目介绍
+            image:['works/9.jpg','works/10.jpg','works/11.jpg','works/12.jpg'], //项目图片地址
+            link:'mingjixiaohui.github.io/she/' //项目链接
+        },
+        {
+            name:'微信场景',  //项目名称
+            date:'2015.5', //项目时间
+            detail:'移动端展示。这是一个微信场景，微信作为移动端流量的入口，文字，语音等人们太过于熟悉，交互性强，绚丽并具有趣味的微场景成为移动端展示的主力，广泛被人们传播.个人作品用于展示', //项目介绍
+            image:['works/13.jpg','works/14.jpg','works/15.jpg','works/16.jpg'], //项目图片地址
+            link:'mingjixiaohui.github.io/weixin/' //项目链接
+        },
+        {
+            name:'100度乐享',  //项目名称
+            date:'2015.5', //项目时间
+            detail:'这是一站式吃喝玩乐的电商型网站，只要采用了HTML+CSS+JavaScript完成,页面清晰，并具有一定的交互性，个人比较喜欢', //项目介绍
+            image:['works/17.jpg','works/18.jpg','works/19.jpg','works/20.jpg'], //项目图片地址
+            link:'mingjixiaohui.github.io/100/' //项目链接
+        },
+        {
+            name:'Apple Watch',  //项目名称
+            date:'2015.3', //项目时间
+            detail:'模仿苹果的网站做的一个单页面展示页，高端、大气、上档次,采用技术Move.js和Fullpage并没有用CSS3实现动画效果', //项目介绍
+            image:['works/21.jpg','works/22.jpg','works/23.jpg','works/24.jpg'], //项目图片地址
+            link:'mingjixiaohui.github.io/apple/' //项目链接
+        }
+    ],
+    work: [
+        {
+            company:'创意互动', //公司名称
+            job:'前端开发工程师', //公司职位
+            date:'2014年5月-2016年4月',  //在职时间
+            detail:'在公司主要根据客户需求出效果图设计,效果图确认再进行切图与前端JS效果的实现。负责网站沟通，网站设计，网页制作，网站测试，网站上线', //工作介绍
+            logo:'works/company2.jpg' //公司logo地址
+        }
+    ],
+    myInfo:{
+        name:'贾辉', //姓名
+        slogan:'2年开发经验', //相关工作经验
+        edu:'大专', //学历
+        time:'2年', //工作经验
+        sex:'1994年 男', //出生年份及性别
+        city:'北京', //所在城市
+        phone:'18632976376', //电话
+        mail:'923139421@qq.com',  //邮箱
+        work:'WEB前端开发工程师，全职，北京，15K', //期望职位 工作性质 期望工作地点 期望薪资
+        work1:'WEB前端开发工程师', //期望职位
+        nature:'全职',  //工作性质
+        hopeCity:'北京',  //期望工作地点
+        money:'15K',  //期望薪资
+        head:'works/user.jpg',  //头像
+        ps:'未婚'  //备注
+    }
+
+};
